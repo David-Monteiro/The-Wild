9ac5789c07c3ba15aee1222a07a25278360509ad
David Monteiro <david.monteiro2@mail.dcu.ie>
2016-01-20 20:07:22 +0000

    Added testing sprites, scenes and primary classes

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public float speed = 10f;
+    public Vector2 maxVelocity = new Vector2(3, 5);
+
+	void Update () {
+        var forceX = 0f;
+        var forceY = 0f;
+
+        var absVelX = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x;
+        if(Input.GetKey("up")) {
+            if (absVelX < maxVelocity.x) {
+                forceX = speed;
+            }
+        } else if (Input.GetKey("down")){
+            if (absVelX < maxVelocity.x){
+                forceX = speed;
+            }
+        }
+
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(forceX, forceY));
+    }
+}
