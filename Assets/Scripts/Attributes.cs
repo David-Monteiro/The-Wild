2c382ef3fd8c168d6abf63342025ff315738b170
David Monteiro <david.monteiro2@mail.dcu.ie>
2016-03-18 17:38:05 +0000

    attributes class is created

---
+++ Assets/Scripts/Attributes.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Attributes 
+{
+    private Dictionary<string, float> values = new Dictionary<string, float>();
+
+    public Attributes()
+    {
+        values.Add("strenght", 1111111);
+        values.Add("agility", 1111111);
+        values.Add("stamina", 1111111);
+        values.Add("speed", 1111111);
+        values.Add("attackSpeed", 1111111);
+        values.Add("defenceSpeed", 1111111);
+        values.Add("regeneration", 1111111);
+        values.Add("aggresion", 1111111);
+        values.Add("bravery", 1111111);
+        values.Add("vision", 1111111);
+        values.Add("height", 1111111);
+        values.Add("weight", 1111111);
+    }
+
+    public void printAttributes()
+    {
+        Console.WriteLine("strenght" + values["strenght"]);
+        Console.WriteLine("agility" + values["agility"]);
+        Console.WriteLine("stamina" + values["stamina"]);
+        Console.WriteLine("speed" + values["strenspeedght"]);
+        Console.WriteLine("attackSpeed" + values["attackSpeed"]);
+        Console.WriteLine("defenceSpeed" + values["defenceSpeed"]);
+        Console.WriteLine("regeneration" + values["regeneration"]);
+        Console.WriteLine("aggresion" + values["aggresion"]);
+        Console.WriteLine("bravery" + values["bravery"]);
+        Console.WriteLine("vision" + values["vision"]);
+        Console.WriteLine("height" + values["height"]);
+        Console.WriteLine("weight" + values["weight"]);
+    }
+    public void setAttributes(string key, float value)
+    {
+        values[key] = value;
+    }
+
+    public float getAttribute(string attr_name)
+    {
+        float value;
+        if (values.TryGetValue(attr_name, out value)) return value;
+        else return -1;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
+    private Attributes attributes = new Attributes();
 
     public float movementSpeed;
     public float rotationSpeed;
-    private Vector2 maxVelocity = new Vector2(3, 5);
 
     public Transform sightStart0, sightEnd0;
     public Transform sightStart1, sightEnd1;
@@ -31,6 +31,8 @@ public class Player : MonoBehaviour
 
     //GameObject enemy;
 
+
+
     void Start()
     {
         isMoving_flag = false;
@@ -47,9 +49,8 @@ public class Player : MonoBehaviour
     {
         RayCasting();
         //controlledMov();
-        randomMov();
+        randomMov1();
 
-        //Debug.Log(head.position);
 
         //goToLocation(new Vector2(enemy.transform.position.x, enemy.transform.position.y));
 
@@ -58,15 +59,11 @@ public class Player : MonoBehaviour
             turnLeft(25);
         }*/
 
-        //lookLeft error
-        //lookAround eroor
-
     }
 
     void randomMov()
     {
-        //Here I will create random generated actions
-        //I will probabily do it in turns of moving forward/ backwards and rotating left/right
+
         if (!isRotating_flag || !isMoving_flag)
         {
             if (decisionNo > 4) decisionNo = Random.Range(0, 4);
@@ -77,13 +74,40 @@ public class Player : MonoBehaviour
             }
             Debug.Log("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
             makeDecision();
-            
+
         }
         else if (isRotating_flag)
         {
             makeDecision();
         }
-        else if (isMoving_flag) {
+        else if (isMoving_flag)
+        {
+            makeDecision();
+        }
+    }
+
+    void randomMov1()
+    {
+        //Here I will create random generated actions
+        //I will probabily do it in turns of moving forward/ backwards and rotating left/right
+        if (!isRotating_flag || !isMoving_flag)
+        {
+            if (decisionNo > 4) decisionNo = Random.Range(0, 2);
+            else
+            {
+                decisionNo = Random.Range(8, 10);
+                getAnglePos();
+            }
+            Debug.Log("ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
+            makeDecision();
+
+        }
+        else if (isRotating_flag)
+        {
+            makeDecision();
+        }
+        else if (isMoving_flag)
+        {
             makeDecision();
         }
     }
@@ -121,57 +145,62 @@ public class Player : MonoBehaviour
         }
     }
 
-    void smallMoveForward() {
-        if (!isMoving_flag) {
+    void smallMoveForward()
+    {
+        if (!isMoving_flag)
+        {
             locationTarget = frontPointA.position;
         }
         moveTowardsPoint(locationTarget);
 
         if (locationTarget == transform.position) isMoving_flag = false;
-        else if (Physics2D.Linecast(transform.position, head.position, 1 
-            << LayerMask.NameToLayer("Block"))) isMoving_flag = false;
+        else if (checkForObstacle("inFront"))   isMoving_flag = false;
         else isMoving_flag = true;
     }
-    void smallMoveBackward() {
-        if (!isMoving_flag) {
+    void smallMoveBackward()
+    {
+        if (!isMoving_flag)
+        {
             locationTarget = backPointA.position;
         }
         moveTowardsPoint(locationTarget);
 
         if (locationTarget == transform.position) isMoving_flag = false;
-        else if (Physics2D.Linecast(transform.position, tail.position, 1
-            << LayerMask.NameToLayer("Block")))     isMoving_flag = false;
+        else if (checkForObstacle("behind")) isMoving_flag = false;
         else isMoving_flag = true;
     }
 
-    void bigMoveForward() {
-        if (!isMoving_flag) {
+    void bigMoveForward()
+    {
+        if (!isMoving_flag)
+        {
             locationTarget = frontPointB.position;
             //Debug.Log("From: " + transform.position + " , To: " + locationTarget);
         }
         moveTowardsPoint(locationTarget);
 
-        if(locationTarget == transform.position)    isMoving_flag = false;
-        else if (Physics2D.Linecast(transform.position, head.position, 1
-            << LayerMask.NameToLayer("Block")))     isMoving_flag = false;
-        else    isMoving_flag = true;
+        if (locationTarget == transform.position) isMoving_flag = false;
+        else if (checkForObstacle("inFront")) isMoving_flag = false;
+        else isMoving_flag = true;
     }
-    void bigMoveBackward() {
-        if (!isMoving_flag)  {
+    void bigMoveBackward()
+    {
+        if (!isMoving_flag)
+        {
             locationTarget = backPointB.position;
             //Debug.Log("From: " + transform.position + " , To: " + locationTarget);
         }
         moveTowardsPoint(locationTarget);
 
         if (locationTarget == transform.position) isMoving_flag = false;
-        else if (Physics2D.Linecast(transform.position, head.position, 1
-            << LayerMask.NameToLayer("Block")))     isMoving_flag = false;
+        else if (checkForObstacle("behind")) isMoving_flag = false;
         else isMoving_flag = true;
     }
 
 
     //
-    bool turnLeft(float angleTurn){
+    bool turnLeft(float angleTurn)
+    {
         float angle = ((anglePos + angleTurn) % 360);
 
         if (leftRotDone_flag == false)
@@ -181,13 +210,15 @@ public class Player : MonoBehaviour
         isRotating_flag = !((int)transform.rotation.eulerAngles.z == (int)angle);
         Debug.Log(isRotating_flag);
         Debug.Log("Angle " + (int)transform.eulerAngles.z);
-        if(!isRotating_flag){
+        if (!isRotating_flag)
+        {
             leftRotDone_flag = false;
         }
         return !isRotating_flag;
 
     }
-    bool turnRight(float angleTurn){
+    bool turnRight(float angleTurn)
+    {
         int angle = (int)((anglePos + (360 - angleTurn)) % 360);
 
         if (rightRotDone_flag == false)
@@ -197,7 +228,8 @@ public class Player : MonoBehaviour
         Debug.Log(isRotating_flag);
         Debug.Log("Angle " + (int)transform.eulerAngles.z);
 
-        if (!isRotating_flag) {
+        if (!isRotating_flag)
+        {
             rightRotDone_flag = false;
         }
         return !isRotating_flag;
@@ -228,13 +260,15 @@ public class Player : MonoBehaviour
             rotZAngles(myTarget);
             leftRotDone_flag = (int)transform.rotation.eulerAngles.z == (int)myTarget;
         }
-        else if (rightRotDone_flag == false && leftRotDone_flag == true) {
+        else if (rightRotDone_flag == false && leftRotDone_flag == true)
+        {
             rotZAngles(initialPos);
             rightRotDone_flag = (int)transform.rotation.eulerAngles.z == (int)initialPos;
         }
         isRotating_flag = !(rightRotDone_flag && leftRotDone_flag);
 
-        if (!isRotating_flag) {
+        if (!isRotating_flag)
+        {
             rightRotDone_flag = false;
             leftRotDone_flag = false;
             return true;
@@ -245,17 +279,20 @@ public class Player : MonoBehaviour
     {
         float myTarget = (int)((initialPos + (360 - 60)) % 360);
 
-        if (rightRotDone_flag == false) {
+        if (rightRotDone_flag == false)
+        {
             rotZAngles(myTarget);
             rightRotDone_flag = (int)transform.rotation.eulerAngles.z == (int)myTarget;
         }
-        else if (rightRotDone_flag == true && leftRotDone_flag == false) {
+        else if (rightRotDone_flag == true && leftRotDone_flag == false)
+        {
             rotZAngles(initialPos);
             leftRotDone_flag = (int)transform.rotation.eulerAngles.z == (int)initialPos;
         }
 
         isRotating_flag = !(rightRotDone_flag && leftRotDone_flag);
-        if (!isRotating_flag) {
+        if (!isRotating_flag)
+        {
             rightRotDone_flag = false;
             leftRotDone_flag = false;
             return true;
@@ -326,13 +363,19 @@ public class Player : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.x, target.y, target.z), movementSpeed * Time.deltaTime);
     }
 
-    void goToLocation(Vector2 target_pos) {
+    void goToLocation(Vector2 target_pos)
+    {
         target_pos.x = target_pos.x - transform.position.x;
         target_pos.y = target_pos.y - transform.position.y;
         float angle = Mathf.Atan2(target_pos.y, target_pos.x) * Mathf.Rad2Deg;
         angle = angle - 90;
         if (angle > 180) turnRight(angle);
         else turnLeft(angle);
+        if (angle == 0)
+        {
+            locationTarget = new Vector3(target_pos.x, target_pos.y, 0);
+            moveTowardsPoint(locationTarget);
+        }
 
         //I need to improve this function
         //Here the 
@@ -354,6 +397,20 @@ public class Player : MonoBehaviour
         Debug.DrawLine(sightStart0.position, sightEnd2.position, Color.green);
     }
 
+    bool checkForObstacle(string str) {
+        if (str.Equals("behind")) {
+            return (Physics2D.Linecast(transform.position, tail.position, 1 << LayerMask.NameToLayer("Block"))
+                || Physics2D.Linecast(transform.position, backPointA.position, 1 << LayerMask.NameToLayer("Block"))
+                || Physics2D.Linecast(transform.position, tail.position, 1 << LayerMask.NameToLayer("wall_block"))
+                || Physics2D.Linecast(transform.position, backPointA.position, 1 << LayerMask.NameToLayer("wall_block")));
+        }
+        else {
+            return (Physics2D.Linecast(transform.position, head.position, 1 << LayerMask.NameToLayer("Block"))
+                || Physics2D.Linecast(transform.position, frontPointA.position, 1 << LayerMask.NameToLayer("Block"))
+                || Physics2D.Linecast(transform.position, head.position, 1 << LayerMask.NameToLayer("wall_block"))
+                || Physics2D.Linecast(transform.position, frontPointA.position, 1 << LayerMask.NameToLayer("wall_block")));
+        }
+    }
     float getAnglePos()
     {
         if (isRotating_flag == false) { anglePos = transform.eulerAngles.z; }
@@ -369,31 +426,21 @@ public class Player : MonoBehaviour
 
     void makeDecision()
     {
-        if (decisionNo == 0)
-        {
-
-                smallMoveForward();
-
+        if (decisionNo == 0) {
+            smallMoveForward();
             Debug.Log("smallMoveForward");
         }
-        else if (decisionNo == 1)
-        {
-
-                smallMoveBackward();
-
-            Debug.Log("smallMoveBackward");
+        else if (decisionNo == 1) {
+            bigMoveForward();
+            Debug.Log("bigMoveForward");
         }
         else if (decisionNo == 2)
         {
-
-                bigMoveForward();
-
-            Debug.Log("bigMoveForward");
+            smallMoveBackward();
+            Debug.Log("smallMoveBackward");
         }
         else if (decisionNo == 3) {
-
-                bigMoveBackward();
-
+            bigMoveBackward();
             Debug.Log("bigMoveBackward");
         }
         else if (decisionNo == 4)
@@ -424,41 +471,21 @@ public class Player : MonoBehaviour
             {
                 target = Random.Range(4, 45);
                 Debug.Log("turn left");
-                Debug.Log(anglePos + " > " + (anglePos + target)%360 + " XXXXXXXXXXXXXXXXXXXXX");
+                Debug.Log(anglePos + " > " + (anglePos + target) %360 + " XXXXXXXXXXXXXXXXXXXXX");
             }
-            turnLeft(target);
+            turnLeft(45);
         }
-        else if (decisionNo == 9)
-        {
+        else if (decisionNo == 9) {
             if (!isRotating_flag)   
             {
                 target = Random.Range(4, 45);
                 Debug.Log("turn right");
-                Debug.Log(anglePos + " > " + (anglePos-target)%360 + " XXXXXXXXXXXXXXXXXXXXX");
+                Debug.Log(anglePos + " > " + (anglePos- target) %360 + " XXXXXXXXXXXXXXXXXXXXX");
             }
-            turnRight(target);    
+            turnRight(45);    
         }
         else { }
     }
 
-    void xSecondsPause() {
-        StartCoroutine("Wait");
-    }
 
-    IEnumerator Wait() {
-        Debug.Log("Pause from >>>>>>>>>> " + Time.time);
-        yield return new WaitForSeconds(2);
-    }
-
-    Vector3 locationDecider() {
-        Vector3 pointA = head.position;
-        Vector3 pointB = tail.position;
-        Debug.Log(new Vector3(pointA.x * pointB.x, pointA.y * pointB.y, 0));
-        return new Vector3(pointA.x * pointB.x, pointA.y * pointB.y, 0);   
-    }
-
-}
-
-class playerController : MonoBehaviour {
-    
 }
