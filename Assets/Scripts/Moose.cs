4da155a3996373111aa09f0d6d5b7e27ea6f5b83
David Monteiro <david.monteiro2@mail.dcu.ie>
2016-05-21 14:55:58 +0100

    Moose creation: sprite, animation and script added to a new prefab

---
+++ Assets/Scripts/Moose.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moose : Herbivorous
+{
+
+    public new void Start()
+    {
+        base.Start();
+
+
+    }
+
+    public new void Update()
+    {
+        base.Update();
+        Animator.SetInteger("Animation_Rotation_State", (int)transform.rotation.eulerAngles.z);
+        Animator.SetBool("Animation_Mov_State", isMoving_flag);
+
+        RandomMov();
+    }
+
+
+}
